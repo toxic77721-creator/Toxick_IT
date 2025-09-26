dbe01a36d095249e3eaae0b8a160b60a0d882fb2
Oleh <toxic7777@gmail.com>
2025-09-26 13:36:38 +0300

    newFolder

--- script.js
+++
@@ -1,8 +0,0 @@
-const startBtn = document.createElement("button");
-startBtn.textContent = "ПлчатокЄ";
-
-document.body.appendChild(srartBtn);
-
-startBtn.addEventListener("click", () => {
-    window.location.href = "game.html";
-});
